80d446d0bf135a7fc00245fccf11df80ba605c38
DSC <sachev.andrei@gmail.com>
2024-11-09 22:44:50 +0500

    Can generate from one file

---
+++ modules/employees/ai.js
@@ -0,0 +1,91 @@
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+const { GoogleAIFileManager } =  require('@google/generative-ai/server');
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+const fs = require('fs');
+const libre = require('libreoffice-convert');
+
+
+// let lib_convert = promisify(libre.convert);
+
+const config = require('../../config');
+
+const genAI = new GoogleGenerativeAI(config.secrets.ai_token);
+const fileManager = new GoogleAIFileManager(config.secrets.ai_token);
+
+
+async function convert(file_name) {
+	try {
+		const enterPath = path.join(__dirname, `/uploads/${file_name}`);
+		const outputPath = path.join(__dirname, `/uploads/${file_name}`);
+
+		let data = await fs.readFile(enterPath)
+
+		let done = await lib_convert(data, '.pdf', undefined)
+
+		await fs.writeFile(outputPath, done)
+
+		return { success: true, file_name: file_name };
+	} catch (err) {
+		console.log(err)
+		return { success: false }
+	}
+}
+
+module.exports.convert_file = convert;
+
+
+async function analys_cv(file_data) {
+	upload_cv = await fileManager.uploadFile(
+		path.join(__dirname, `../../uploads/${file_data}`),
+		{
+			mimeType: "application/pdf",
+			displayName: "Book",
+		}
+	);
+
+	console.log(`Uploaded file ${upload_cv.file.displayName} as: ${upload_cv.file.uri}`);
+
+	const model = genAI.getGenerativeModel({ "model": "gemini-1.5-flash"});
+
+	const chat = model.startChat({
+		history: []
+	});
+
+	let result = await chat.sendMessage([ 
+		{
+			fileData: {
+				mimeType: upload_cv.file.mimeType,
+				fileUri: upload_cv.file.uri,
+			}
+		} , `Проанализируй этот файл и выдай мне мне в json формате основные данные такие как: имя, фамилия, гендер(f - жещина или m - мужчина), образование, место проживания, может и хочет ли она переезжать(0 - не хочет или не может переезжать, 1 - может переехать, но не хотелось бы, 2 - готова к перездам), дата рождения, телефоный номер, почту, опыт работы(в годах), относительную оценку как сотрудника по 100 бальной системе, скилы(5-6 штук, короткие, записывать в skills), языки которыми владеет и массив со строками где будет краткие тезисы его как сотрудника(должны быть как и hard skills, так и soft skils, не менее 30 тезисов) для будущей индексации и подбора вакансии для него. Если в резюме отсутсвуют какие-то данные пиши их как "none". Пример json:
+{
+"name" : "имя",
+"surname": "фамилия",
+"gender": "m",
+"email": "электроная почта",
+"phone": "номер телефона",
+"education": "данные"
+"locate": "место проживания",
+"relocate": 0,
+"birthday": "dd.mm.yyyy",
+"experience": 0,
+"level": 0,
+"skills": ["данные", "данные"],
+"languages": ["данные", "данные"],
+"index": ["данные" , "данные"]
+}` ]);
+
+	result = (result.response.text()).replace(/```json\s*|\s*```/g, '');
+
+	await fileManager.deleteFile(upload_cv.file.name);
+
+	result.id = file_data.name;
+
+	console.log(result);
+
+	return JSON.parse(result);
+}
+
+
+module.exports.analys_cv = analys_cv;
---
+++ test/send_file.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const FormData = require('form-data');
+
+// Файл, который нужно отправить
+const filePath = './123.pdf';
+
+// Создаём форму и добавляем в нее файл
+const form = new FormData();
+form.append('files', fs.createReadStream(filePath));
+
+// Опции для запроса
+const options = {
+  method: 'POST',
+  host: 'localhost', // Замените на адрес вашего сервера
+  port: 3000,        // Замените на порт вашего сервера
+  path: '/api/v1/employees/cvs/upload',   // Путь на сервере для обработки загрузки
+  headers: form.getHeaders(),
+};
+
+// Создаем запрос и отправляем данные формы
+const req = http.request(options, (res) => {
+  let data = '';
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('end', () => {
+    console.log('Ответ сервера:', data);
+  });
+});
+
+req.on('error', (error) => {
+  console.error('Ошибка запроса:', error);
+});
+
+// Отправляем данные формы
+form.pipe(req);
